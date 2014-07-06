89a11e341626a6719f3e5bc53d2fb7bc42eb4272
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2014-07-06 21:59:00 +0300

    Add grunt and mocha tests

---
+++ Gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      files: ['*.js', 'test/**/*.js'],
+        options: {
+          globals: {
+            jQuery: true,
+            console: true,
+            module: true
+          }
+        }
+    },
+    shell: {
+      mocha: {
+        options: {
+          stdout: true
+        },
+        command: 'mocha'
+      }
+    },
+    release: {
+      options: {
+        // Don't release to NPM since travis does this
+        npm: false,
+        npmtag: false,
+        // default: 'release <%= version %>'
+        commitMessage: 'Release <%= version %>'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-release');
+
+  grunt.registerTask('test', ['jshint', 'shell:mocha']);
+};
