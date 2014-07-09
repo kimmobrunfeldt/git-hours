d48e9a07247ff112723ede1aec055658157721a8
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2014-07-09 18:17:42 +0300

    Crawl through all branches for commits instead of just master

--- index.js
+++ index.js
@@ -5,14 +5,17 @@ var git = require('nodegit');
 var moment = require('moment');
 var _ = require('lodash');
 
+var exec = Promise.promisify(require('child_process').exec);
+
 var config = {
     // Maximum time diff between 2 subsequent commits in minutes which are
     // counted to be in the same coding "session"
-    maxCommitDiffInMinutes: 240,
+    maxCommitDiffInMinutes: 2 * 60,
 
     // How many minutes should be added for the first commit of coding session
-    firstCommitAdditionInMinutes: 240
+    firstCommitAdditionInMinutes: 2 * 60
 };
+
 
 function main() {
     commits('.').then(function(commits) {
@@ -59,9 +62,9 @@ function estimateHours(dates) {
     return Math.round(hours);
 }
 
-
 // Promisify nodegit's API of getting all commits in repository
 function commits(gitPath) {
+    // Promisifing nodegit did not work.
     return new Promise(function(resolve, reject) {
         git.Repo.open(gitPath, function(err, repo) {
             if (err) {
@@ -69,42 +72,99 @@ function commits(gitPath) {
                 return;
             }
 
-            repo.getMaster(function(err, branch) {
-                if (err) {
-                    reject(err);
-                    return;
-                }
-
-                var history = branch.history();
-                var commits = [];
-
-                history.on("commit", function(commit) {
-                    var author = null;
-                    if (!_.isNull(commit.author())) {
-                        author = {
-                            name: commit.author().name(),
-                            email: commit.author().email()
-                        };
-                    }
-
-                    var commitData = {
-                        sha: commit.sha(),
-                        date: commit.date(),
-                        message: commit.message(),
-                        author: author
-                    };
-
-                    commits.push(commitData);
+            // Get all commits for all branches
+            getBranchNames(gitPath).map(function(branchName) {
+                console.log(branchName);
+                return getBranch(repo, branchName);
+            }).map(function(branch) {
+                console.log(branch);
+                return getBranchCommits(branch);
+            }).reduce(function(allCommits, branchCommits) {
+                _.each(branchCommits, function(commit) {
+                    allCommits.push(commit);
                 });
 
-                history.on("end", function() {
-                    resolve(commits);
+                return allCommits;
+            }, []).then(function(commits) {
+                var uniqueCommits = _.uniq(commits, function(item, key, a) {
+                    return item.sha;
                 });
 
-                // Start emitting events.
-                history.start();
-            });
+                resolve(uniqueCommits);
+            }).catch(reject);
         });
+    });
+}
+
+function getBranchNames(gitPath) {
+    var cmd = "git branch --no-color | awk -F ' +' '! /\(no branch\)/ {print $2}'";
+    return exec(cmd, {cwd: gitPath}).then(function(stdout, stderr) {
+        if (stderr) {
+            throw new Error(stderr);
+        }
+
+        return stdout
+                .filter(function(e) { return e; })  // Remove empty
+                .map(function(str) { return str.trim(); });  // Trim whitespace
+    });
+}
+
+function getBranches(repo, names) {
+    var branches = [];
+    for (var i = 0; i < names.length; ++i) {
+        branches.push(getBranch(repo, names[i]));
+    }
+
+    return Promise.all(branches);
+}
+
+function getBranch(repo, name) {
+    return new Promise(function(resolve, reject) {
+        repo.getBranch(name, function(err, branch) {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve(branch);
+        });
+    });
+}
+
+function getBranchCommits(branch) {
+    return new Promise(function(resolve, reject) {
+        var history = branch.history();
+        var commits = [];
+
+        history.on("commit", function(commit) {
+            var author = null;
+            if (!_.isNull(commit.author())) {
+                author = {
+                    name: commit.author().name(),
+                    email: commit.author().email()
+                };
+            }
+
+            var commitData = {
+                sha: commit.sha(),
+                date: commit.date(),
+                message: commit.message(),
+                author: author
+            };
+
+            commits.push(commitData);
+        });
+
+        history.on("end", function() {
+            resolve(commits);
+        });
+
+        history.on("error", function(err) {
+            reject(err)
+        });
+
+        // Start emitting events.
+        history.start();
     });
 }
 
