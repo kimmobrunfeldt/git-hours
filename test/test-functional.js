1d17258e4788e09d069878ed37a94e7cf5e723ec
Piero Blunda <pieroblunda@gmail.com>
2015-09-20 04:16:10 -0300

    Doing test

--- test/test-functional.js
+++ test/test-functional.js
@@ -2,6 +2,8 @@ var _ = require('lodash');
 var assert = require("assert");
 var exec = require('child_process').exec;
 
+var totalHoursCount;
+
 describe('git-hours', function() {
 
     describe('cli', function() {
@@ -16,9 +18,58 @@ describe('git-hours', function() {
                 var work = JSON.parse(stdout);
                 assert.notEqual(work.total.hours.length, 0);
                 assert.notEqual(work.total.commits.length, 0);
-
+                totalHoursCount = work.total.hours;
                 done();
             });
         });
     });
+
+    describe('Since option', function(){
+      it('Should analyse since today', function(done) {
+        exec('node index.js --since today', function(err, stdout, stderr) {
+          assert.ifError(err);
+          var work = JSON.parse(stdout);
+          assert.strictEqual(typeof work.total.hours, 'number');
+          done();
+        });
+      });
+
+      it('Should analyse since yesterday', function(done) {
+        exec('node index.js --since yesterday', function(err, stdout, stderr) {
+          assert.ifError(err);
+          var work = JSON.parse(stdout);
+          assert.strictEqual(typeof work.total.hours, 'number');
+          done();
+        });
+      });
+
+      it('Should analyse since last week', function(done){
+        exec('node index.js --since lastweek', function(err, stdout, stderr) {
+          assert.ifError(err);
+          var work = JSON.parse(stdout);
+          assert.strictEqual(typeof work.total.hours, 'number');
+          done();
+        });
+      });
+
+      it('Should analyse since a specific date', function(done){
+        exec('node index.js --since 2015-01-01', function(err, stdout, stderr) {
+          assert.ifError(err);
+          var work = JSON.parse(stdout);
+          assert.notEqual(work.total.hours, 0);
+          done();
+        });
+      });
+
+      it('Should analyse as without param', function(done){
+        exec('node index.js --since always', function(err, stdout, stderr) {
+          assert.ifError(err);
+          var work = JSON.parse(stdout);
+          assert.equal(work.total.hours, totalHoursCount);
+          done();
+        });
+      });
+    });
+
+
 });
